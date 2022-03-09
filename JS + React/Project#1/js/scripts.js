fd9707ee2124663f4d14c3b2daae2c95c3f64ef7
Anton <superantoxa12@gmail.com>
2022-03-09 20:35:42 +0100

    Add checking for prompt and delete some bugs

--- JS + React/Project#1/js/scripts.js
+++ JS + React/Project#1/js/scripts.js
@@ -1,16 +1,46 @@
 
 
-let numbersOfFilms = +prompt("Сколько фильмов вы посмотрели?");
-let lastFilm = prompt("Один из последних ваших фильмов?");
-let ratingLastFilm = prompt("На сколько его оцените?");
+let numbersOfFilms;
+let lastFilm = "";
+let ratingLastFilm;
+
+
+do{
+    numbersOfFilms = +prompt("Сколько фильмов вы посмотрели?");
+
+}while(numbersOfFilms == "" || numbersOfFilms == null){}
+
+do{
+    lastFilm = prompt("Один из последних ваших фильмов?");
+
+}while(lastFilm == "" || lastFilm == null || lastFilm.length > 50){}
+
+do{
+    ratingLastFilm = prompt("На сколько его оцените?");
+
+}while(ratingLastFilm == "" || ratingLastFilm == null || ratingLastFilm.length > 50){}
 
 let personalMovieDB = {
     count : numbersOfFilms,
-    movies : {
-        lastFilm : ratingLastFilm
-    },
+    movies : {},
     actors : {},
     genres : [],
     privat : false
 };
+
+
+personalMovieDB.movies[lastFilm] = ratingLastFilm;
+
+switch (true) {
+    case (personalMovieDB.count < 10) : alert("Просмотрено мало фильмов");        
+        break;
+    case (personalMovieDB.count >= 10 && personalMovieDB.count < 30) : alert("Классический зритель");
+        break;
+    case (personalMovieDB.count >= 30) : alert("Вы киноман!");
+        break;
+    default : alert("Ошибочка!");
+      break;
+}
+
+
 console.log(personalMovieDB);
